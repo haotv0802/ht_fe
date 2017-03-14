d4e1d5cf0df60056399583d44514a3ef7d7c26b4
HaoHo <haho@codix.eu>
2017-03-14 17:12:00 +0700

    Implement Login page.

---
+++ app/common/login/credential.ts
@@ -0,0 +1,5 @@
+export interface ICredential {
+    user: number;
+    pass: string;
+    lang: string;
+}
---
+++ app/common/login/login.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from 'angular2/core';
+import { ICredential } from "./credential";
+import { Http, Response } from 'angular2/http';
+
+@Injectable()
+export class LoginService {
+    constructor(private _http: Http) {
+    }
+
+    displayLoginPage() {
+
+    }
+
+    login() {
+
+    }
+
+}
