293bc0d7cf92d97b26e4bec8d1bc12eeb2420140
HaoHo <haho@codix.eu>
2017-03-16 16:48:57 +0700

    add Staff component.

---
+++ app/staff/staff.service.js
@@ -0,0 +1,13 @@
+// import { Injectable } from 'angular2/core';
+// import { Http, Response } from 'angular2/http';
+// import { Observable } from 'rxjs/Observable';
+// import { Headers } from "angular2/src/http/headers";
+//
+// @Injectable()
+// export class StaffService {
+//
+//     constructor (private _http: Http) {
+//
+//     }
+// }
+//# sourceMappingURL=staff.service.js.map
