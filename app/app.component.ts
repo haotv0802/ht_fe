dc9047c610bc0e47632911316f20918391f65333
HaoHo <haho@codix.eu>
2017-03-26 15:46:25 +0700

    Move Welcome component to common folder.

--- app/app.component.ts
+++ app/app.component.ts
@@ -21,6 +21,7 @@ import { Component } from '@angular/core';
     //  `
     templateUrl: 'app/app.component.html'
 })
+
 export class AppComponent {
     // pageTitle: string = 'Acme Product Management';
 }
