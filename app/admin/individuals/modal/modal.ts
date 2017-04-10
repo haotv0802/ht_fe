eecc5a715767b644061ef857d8b89a4c4a3070d9
HaoHo <haho@codix.eu>
2017-04-10 11:51:32 +0700

    Adjust name of Libraries (because of different versions of Angular)

--- app/admin/individuals/modal/modal.ts
+++ app/admin/individuals/modal/modal.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import {Component, ComponentResolver, ViewChild,
-  ViewContainerRef, ViewEncapsulation, ComponentRef, DynamicComponentLoader,
-  ElementRef, Input, EventEmitter, Output} from '@angular/core';
+import {
+  Component, ComponentFactoryResolver, ViewChild,
+  ViewContainerRef, ViewEncapsulation, ComponentRef,
+  ElementRef, Input, EventEmitter, Output
+} from '@angular/core';
 import {Open} from './open.component';
 
 @Component({
@@ -34,114 +36,117 @@ import {Open} from './open.component';
   </div>
 `,
   providers: [],
-  directives: [Open],
-  encapsulation: ViewEncapsulation.None,
-  pipes: []
+  encapsulation: ViewEncapsulation.None
 })
 /**
-  * API to an open modal window.
-  */
-export class Modal{
+ * API to an open modal window.
+ */
+export class Modal {
   /**
-     * Caption for the title.
-     */
-  public modalTitle:string;
+   * Caption for the title.
+   */
+  public modalTitle: string;
   /**
-    * component which is to be loaded dynamically.
-    */
-  public component:any;
+   * component which is to be loaded dynamically.
+   */
+  public component: any;
   /**
-     * Describes if the modal contains Ok Button.
-     * The default Ok button will close the modal and emit the callback.
-     * Defaults to true.
-     */
-  public okButton:boolean = true;
+   * Describes if the modal contains Ok Button.
+   * The default Ok button will close the modal and emit the callback.
+   * Defaults to true.
+   */
+  public okButton: boolean = true;
   /**
-     * Caption for the OK button.
-     * Default: Ok
-     */
-  public okButtonText:string= 'Ok';
+   * Caption for the OK button.
+   * Default: Ok
+   */
+  public okButtonText: string = 'Ok';
   /**
-     * Describes if the modal contains cancel Button.
-     * The default Cancelbutton will close the modal.
-     * Defaults to true.
-     */
-  public cancelButton:boolean = true;
+   * Describes if the modal contains cancel Button.
+   * The default Cancelbutton will close the modal.
+   * Defaults to true.
+   */
+  public cancelButton: boolean = true;
   /**
-     * Caption for the Cancel button.
-     * Default: Cancel
-     */
-  public cancelButtonText:string = 'Cancel';
+   * Caption for the Cancel button.
+   * Default: Cancel
+   */
+  public cancelButtonText: string = 'Cancel';
   /**
-     * if the modalMessage is true it will show the message inside modal body.
-     */
-  public modalMessage:boolean = true;
+   * if the modalMessage is true it will show the message inside modal body.
+   */
+  public modalMessage: boolean = true;
   /**
-     * Some message/content can be set in message which will be shown in modal body.
-     */
-  public message:string;
+   * Some message/content can be set in message which will be shown in modal body.
+   */
+  public message: string;
   /**
-    * if the value is true modal footer will be visible or else it will be hidden.
-    */
-  public modalFooter:boolean= true;
+   * if the value is true modal footer will be visible or else it will be hidden.
+   */
+  public modalFooter: boolean = true;
   /**
-    * shows modal header if the value is true.
-    */
-  public modalHeader:boolean = true;
+   * shows modal header if the value is true.
+   */
+  public modalHeader: boolean = true;
   /**
-    * if the value is true modal will be visible or else it will be hidden.
-    */
-  public isOpen:boolean=false;
+   * if the value is true modal will be visible or else it will be hidden.
+   */
+  public isOpen: boolean = false;
   /**
-    * Emitted when a ok button was clicked
-    * or when close method is called.
-    */
-  cmpRef:ComponentRef<any>;
-  @ViewChild("child", {read: ViewContainerRef}) target;
-  @Output() public modalOutput:EventEmitter<any> = new EventEmitter();
-  constructor(public compiler: ComponentResolver, public viewContainer: ViewContainerRef){
+   * Emitted when a ok button was clicked
+   * or when close method is called.
+   */
+  cmpRef: ComponentRef<any>;
+  @ViewChild("child", {read: ViewContainerRef}) target: any;
+  @Output() public modalOutput: EventEmitter<any> = new EventEmitter();
+
+  constructor(public compiler: ComponentFactoryResolver, public viewContainer: ViewContainerRef) {
   }
+
   /**
-       * Opens a modal window creating backdrop.
-       * @param component The angular Component that is to be loaded dynamically(optional).
-       */
-  open(component?){
-    this.isOpen= true;
-    if(this.cmpRef) {
+   * Opens a modal window creating backdrop.
+   * @param component The angular Component that is to be loaded dynamically(optional).
+   */
+  open(component?: any) {
+    this.isOpen = true;
+    if (this.cmpRef) {
       this.cmpRef.destroy();
     }
-    if(component){
-        this.compiler.resolveComponent(component).then((factory) => 
-            this.cmpRef = this.target.createComponent(factory));
+    if (component) {
+      let componentFactory = this.compiler.resolveComponentFactory(component);
+      this.cmpRef = this.target.createComponent(componentFactory);
     }
   }
+
   /**
-     *  close method dispose the component, closes the modal and optionally emits modalOutput value.
-     */
-  close(data?:any){
+   *  close method dispose the component, closes the modal and optionally emits modalOutput value.
+   */
+  close(data?: any) {
     this.dispose();
-    if(data){
+    if (data) {
       this.modalOutput.emit(data);
     }
   }
+
   /**
-     *  ok method dispose the component, closes the modal and emits true.
-     */
-  submit(){
+   *  ok method dispose the component, closes the modal and emits true.
+   */
+  submit() {
     this.dispose();
     this.modalOutput.emit(true);
   }
+
   /**
-     *  dispose method dispose the loaded component.
-     */
-  dispose(){
+   *  dispose method dispose the loaded component.
+   */
+  dispose() {
     this.isOpen = false;
-    if(this.component != undefined){
-           this.component.then((componentRef:ComponentRef<any>) => {
-           componentRef.destroy();
-           return componentRef;
-         });
-        }
+    if (this.component != undefined) {
+      this.component.then((componentRef: ComponentRef<any>) => {
+        componentRef.destroy();
+        return componentRef;
+      });
+    }
   }
 
 }
